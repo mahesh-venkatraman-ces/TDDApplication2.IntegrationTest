ee40434df828504eb6f5e6c9a6698da35e0d67c0
Mahesh Venkatraman <mahesh.venkatraman@cesltd.com>
2023-10-17 16:23:32 +0530

    Add project files.

---
+++ TDDApplication2.IntegrationTest/Services/UserServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+
+namespace TDDApplication2.IntegrationTest.Services
+{
+    public class UserServiceTests
+    {        
+
+        public UserServiceTests()
+        {
+            _userService = new UserService(_userRepository.Object);
+        }
+
+        [Theory]
+        [AutoData]
+        public async Task GetUsersAsync_WhenSuccess_ReturnsUserDTOList(List<User> users)
+        {
+            //Arrange
+            var userEntityList = users;
+
+            _userRepository
+                .Setup(repo => repo.GetListAsync())
+                .ReturnsAsync(userEntityList);
+
+            //Act
+            var result = await _userService.GetUsersAsync();
+
+            //Assert
+            Assert.Equal(1, result.Count);
+        }        
+    }
+}
